42619a26c80e77eef86b3b6880a1bc05fefa716b
pauljonadev <jonathandavidpaul.f@gmail.com>
2025-12-08 13:36:53 -0500

    test(ci): mock react-redux Provider y transpilar ESM en Jest para evitar SyntaxError

--- VisionCheckAI/__tests__/App.test.tsx
+++ VisionCheckAI/__tests__/App.test.tsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react-native';
 import App from '../App';
 
 jest.mock('expo-camera', () => ({ useCameraPermissions: () => [ { granted: false }, jest.fn() ] }));
+jest.mock('react-redux', () => ({ Provider: ({ children }: any) => children }));
 
 test('renderiza la app sin errores', () => {
   const tree = render(<App />);
